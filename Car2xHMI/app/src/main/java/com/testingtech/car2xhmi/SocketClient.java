1ebeaea0472274ca231e438dcbf079f1f8c36c8b
FlyingCrafter <paul.kunze@fu-berlin.de>
2015-03-05 13:13:57 +0100

    Enabled UI output according to socket messages

--- Car2xHMI/app/src/main/java/com/testingtech/car2xhmi/SocketClient.java
+++ Car2xHMI/app/src/main/java/com/testingtech/car2xhmi/SocketClient.java
@@ -1,18 +1,26 @@
 package com.testingtech.car2xhmi;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.AsyncTask;
+import android.view.View;
+import android.widget.ScrollView;
+import android.widget.TableLayout;
 import android.widget.TextView;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.net.Socket;
 
-public class SocketClient extends AsyncTask<Void, String, String> {
+public class SocketClient extends AsyncTask<Void, Integer, String> {
 
     TextView textview;
+    ScrollView scrollview;
+    Context context;
 
-    public SocketClient(TextView tv) {
+    public SocketClient(TextView tv, ScrollView sv) {
         textview = tv;
+        scrollview = sv;
     }
 
     @Override
@@ -25,11 +33,12 @@ public class SocketClient extends AsyncTask<Void, String, String> {
             DataOutputStream dos = new DataOutputStream(mySocket.getOutputStream());
 
             dos.writeInt(request);
-            publishProgress("[Client] sent request code [" + request + "]");
 
+            int i = 0;
             do{
                 response = dis.readInt();
-                publishProgress("[Client] received code [" + response + "]");
+                publishProgress(response, i);
+                i++;
             } while (response != 57);
 
             mySocket.close();
@@ -40,13 +49,24 @@ public class SocketClient extends AsyncTask<Void, String, String> {
     }
 
     @Override
-    protected void onProgressUpdate(String... progress) {
+    protected void onProgressUpdate(Integer... progress) {
         super.onProgressUpdate(progress);
-        textview.setText(progress[0]);
+        textview.setText("[Client] received code [" + progress[0] + "]");
+        TableLayout table = (TableLayout) scrollview.getChildAt(0);
+        if(progress[1] > 0) {
+            TextView oldText = (TextView) table.getChildAt(progress[1] - 1);
+            oldText.setBackgroundColor(Color.WHITE);
+        }
+        TextView text = (TextView) table.getChildAt(progress[1]);
+        text.setBackgroundResource(R.drawable.rectangle_border_red);
+        scrollview.smoothScrollTo(0, text.getTop());
     }
 
     @Override
     protected void onPostExecute(String results) {
         textview.setText(results);
+        TableLayout table = (TableLayout) scrollview.getChildAt(0);
+        TextView oldText = (TextView) table.getChildAt(table.getChildCount() - 1);
+        oldText.setBackgroundColor(Color.WHITE);
     }
 }
