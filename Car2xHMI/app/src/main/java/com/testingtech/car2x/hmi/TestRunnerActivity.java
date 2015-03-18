d6ac772a1894303a7996d29df82149dfa3b4ea34
FlyingCrafter <paul.kunze@fu-berlin.de>
2015-03-18 21:44:57 +0100

    Added TextToSpeech for Stages

--- Car2xHMI/app/src/main/java/com/testingtech/car2x/hmi/TestRunnerActivity.java
+++ Car2xHMI/app/src/main/java/com/testingtech/car2x/hmi/TestRunnerActivity.java
@@ -3,6 +3,7 @@ package com.testingtech.car2x.hmi;
 import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,10 +14,19 @@ import android.widget.ProgressBar;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
+import java.util.Locale;
+
 public class TestRunnerActivity extends ActionBarActivity {
 
+    private TextView statusText;
+    private TextView socketConn;
+    private ScrollView progress;
+    private ProgressBar progressBar;
+    private Button btnStart;
+    private Button btnStop;
     private AnimationDrawable logoAnimation;
     private SocketClient socketClient;
+    private TextToSpeech ttobj;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,22 +52,32 @@ public class TestRunnerActivity extends ActionBarActivity {
         ImageView logoImage = (ImageView) findViewById(R.id.status_animation);
         logoImage.setBackgroundResource(R.drawable.animation_logo);
         logoAnimation = (AnimationDrawable) logoImage.getBackground();
+
+        ttobj = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR) {
+                    ttobj.setLanguage(Locale.UK);
+                }
+            }
+        });
+        statusText = (TextView) findViewById(R.id.status_text);
+        socketConn = (TextView) findViewById(R.id.socket);
+        progress = (ScrollView) findViewById(R.id.progress);
+        progressBar = (ProgressBar) findViewById(R.id.progressbar);
+        btnStart = (Button) findViewById(R.id.button_start);
+        btnStop = (Button) findViewById(R.id.button_stop);
     }
 
     public void startTest(View view) {
-        TextView statusText = (TextView) findViewById(R.id.status_text);
-        TextView socketConn = (TextView) findViewById(R.id.socket);
-        ScrollView progress = (ScrollView) findViewById(R.id.progress);
-        ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressbar);
-        Button btnStart = (Button) findViewById(R.id.button_start);
-        Button btnStop = (Button) findViewById(R.id.button_stop);
         socketClient = new SocketClient(this, socketConn, progress, progressBar, logoAnimation,
-                statusText, btnStart, btnStop);
+                statusText, btnStart, btnStop, ttobj);
         socketClient.execute();
     }
 
     public void stopTest(View view) {
         socketClient.cancel(true);
+        socketClient.closeSocket();
     }
 
     @Override
@@ -80,5 +100,12 @@ public class TestRunnerActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        if(ttobj != null)
+            ttobj.shutdown();
     }
 }
