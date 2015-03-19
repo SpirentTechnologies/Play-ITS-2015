ada8d45cc828c64fa0285fddc42ca0de138bd694
FlyingCrafter <paul.kunze@fu-berlin.de>
2015-03-19 15:01:06 +0100

    Added a grouped list in the TestSelector

---
+++ Car2xHMI/app/src/main/java/com/testingtech/car2x/hmi/ExpandableListAdapter.java
@@ -0,0 +1,98 @@
+package com.testingtech.car2x.hmi;
+
+import java.util.List;
+import java.util.Map;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+public class ExpandableListAdapter extends BaseExpandableListAdapter {
+
+    private Activity context;
+    private Map<String, List<String>> groups;
+    private List<String> items;
+
+    public ExpandableListAdapter(Activity context, List<String> items,
+                                 Map<String, List<String>> groups) {
+        this.context = context;
+        this.groups = groups;
+        this.items = items;
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return groups.get(items.get(groupPosition)).get(childPosition);
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public View getChildView(final int groupPosition, final int childPosition,
+                             boolean isLastChild, View convertView, ViewGroup parent) {
+        final String laptop = (String) getChild(groupPosition, childPosition);
+        LayoutInflater inflater = context.getLayoutInflater();
+
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.child_item, null);
+        }
+
+        TextView item = (TextView) convertView.findViewById(R.id.laptop);
+        item.setText(laptop);
+        return convertView;
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return groups.get(items.get(groupPosition)).size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return items.get(groupPosition);
+    }
+
+    @Override
+    public int getGroupCount() {
+        return items.size();
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded,
+                             View convertView, ViewGroup parent) {
+        String itemName = (String) getGroup(groupPosition);
+        if (convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.group_item,
+                    null);
+        }
+        TextView item = (TextView) convertView.findViewById(R.id.laptop);
+        item.setTypeface(null, Typeface.BOLD);
+        item.setText(itemName);
+        return convertView;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+}
--- Car2xHMI/app/src/main/java/com/testingtech/car2x/hmi/TestSelectorActivity.java
+++ Car2xHMI/app/src/main/java/com/testingtech/car2x/hmi/TestSelectorActivity.java
@@ -6,18 +6,77 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ExpandableListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 public class TestSelectorActivity extends ActionBarActivity {
 
     public final static String TEST_NAME = "drive";
+    ArrayList<String> groupList;
+    ArrayList<String> speedItems, doorItems, brakeItems, lightItems, engineItems, wheelItems;
+    Map<String, List<String>> collection;
+    ExpandableListView expListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_selector);
+
+        createGroups();
+        createItems();
+        createCollection();
+        expListView = (ExpandableListView) findViewById(R.id.listView);
+        final ExpandableListAdapter expListAdapter = new ExpandableListAdapter(
+                this, groupList, collection);
+        expListView.setAdapter(expListAdapter);
+
+        expListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                startDriveTest();
+                return true;
+            }
+        });
     }
 
-    public void startDriveTest(View view){
+    private void createGroups() {
+        groupList = new ArrayList<String>();
+        groupList.add("Speed Tests");
+        groupList.add("Door Tests");
+        groupList.add("Break Tests");
+        groupList.add("Light Tests");
+        groupList.add("Engine Tests");
+        groupList.add("Steering Wheel Tests");
+    }
+
+    private void createItems() {
+        speedItems = new ArrayList<>(Arrays.asList("Speed up to 50 & slow down to 30 km/h",
+                "Speed up to 30 & stop", "Drive in reverse"));
+        doorItems = new ArrayList<>(Arrays.asList("Test open door with speed over 50 km/h"));
+        brakeItems = new ArrayList<>(Arrays.asList("Test full brake with speed over 60 km/h",
+                "Activate hand brake", "Test Brakes' efficiency"));
+        lightItems = new ArrayList<>(Arrays.asList("Test headlamp status"));
+        engineItems = new ArrayList<>(Arrays.asList("Test engine rotation speed"));
+        wheelItems = new ArrayList<>(Arrays.asList("Test steering wheel angle"));
+    }
+
+    private void createCollection(){
+        collection = new LinkedHashMap<String, List<String>>();
+        collection.put(groupList.get(0), speedItems);
+        collection.put(groupList.get(1), doorItems);
+        collection.put(groupList.get(2), brakeItems);
+        collection.put(groupList.get(3), lightItems);
+        collection.put(groupList.get(4), engineItems);
+        collection.put(groupList.get(5), wheelItems);
+    }
+
+    public void startDriveTest(){
         startTestRunnerActivity("drive");
     }
 
