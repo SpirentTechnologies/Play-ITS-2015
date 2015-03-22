f26cab28fb1299fad86056ee232cb7b1ead29127
chrisdamm <christian_damm@hotmail.de>
2015-03-22 18:57:42 +0100

    refactored existing testcases

--- TTCN3_Project/javasrc/CANPortFilter/CANPortFilter.java
+++ TTCN3_Project/javasrc/CANPortFilter/CANPortFilter.java
@@ -15,66 +15,69 @@ import org.etsi.ttcn.tri.TriStatus;
 import com.testingtech.ttcn.tri.PortFilter;
 
 public class CANPortFilter extends PortFilter {
-	
-	File speedFile = new File("resource/Speed");
-	BufferedReader br = null;
-	String speed = null;
 
 	private static final long serialVersionUID = -3492721938799363917L;
 
 	/**
 	 * 
-	 * @param tsiPortId Identifies the system port.
-	 * @param SUTAddress Indicates an internal address of the SUT.
-	 * @param componentId Identifies the target component.
-	 * @param receivedMessage Contains the encoded value that is processed by the
-	 *        by the receive statement.
+	 * @param tsiPortId
+	 *            Identifies the system port.
+	 * @param SUTAddress
+	 *            Indicates an internal address of the SUT.
+	 * @param componentId
+	 *            Identifies the target component.
+	 * @param receivedMessage
+	 *            Contains the encoded value that is processed by the by the
+	 *            receive statement.
 	 */
-	public void triEnqueueMsg(TriPortId tsiPortId, TriAddress SUTAddress,
-			TriComponentId componentId, TriMessage receivedMessage) {
-		
+	public void triEnqueueMsg(TriPortId tsiPortId, TriAddress SUTAddress, TriComponentId componentId,
+			TriMessage receivedMessage) {
+
+		File speedFile = new File("resource/Speed");
+		BufferedReader bufferedReader = null;
+		String speed = null;
+
 		// create a new file input stream
 		try {
-			br = new BufferedReader(new FileReader(speedFile));
+			bufferedReader = new BufferedReader(new FileReader(speedFile));
 		} catch (FileNotFoundException e) {
 			System.err.println("Error! File " + speedFile.getAbsoluteFile() + " not found.");
 			e.printStackTrace();
 		}
-		
+
 		// read content from file input stream
-		try {			
-			speed = br.readLine();
+		try {
+			speed = bufferedReader.readLine();
 		} catch (IOException e) {
 			System.err.println("Error while reading file.");
 			e.printStackTrace();
 		}
-		
-		System.out.println("PortFilter " + this
-				+ " sends following outgoing message: "
-				+ speed);
+
+		System.out.println("PortFilter " + this + " sends following outgoing message: " + speed);
 
 		receivedMessage.setEncodedMessage(speed.getBytes());
-		if (receivedMessage.getEncodedMessage().length > 0) { // do not enqueue
-																// zero length
-																// messages
-			super.triEnqueueMsg(tsiPortId, SUTAddress, componentId,
-					receivedMessage);			
+		// FIXME: What is this if-clause for?
+		// do not enqueue zero length messages
+		if (receivedMessage.getEncodedMessage().length > 0) {
+			super.triEnqueueMsg(tsiPortId, SUTAddress, componentId, receivedMessage);
 		}
 	}
 
 	/**
-	 * This method is called when the test case executes a send statement. 
+	 * This method is called when the test case executes a send statement.
 	 * 
-	 * @param componentId Identifies the sending component.
-	 * @param tsiPortId Identifies the system port.
-	 * @param address Target of the send statement.
-	 * @param sendMessage The message to be send.
+	 * @param componentId
+	 *            Identifies the sending component.
+	 * @param tsiPortId
+	 *            Identifies the system port.
+	 * @param address
+	 *            Target of the send statement.
+	 * @param sendMessage
+	 *            The message to be send.
 	 * 
 	 */
-	public TriStatus triSend(TriComponentId componentId, TriPortId tsiPortId,
-			TriAddress address, TriMessage sendMessage) {
-		System.out.println("PortFilter " + this
-				+ " receives following outgoing message: "
+	public TriStatus triSend(TriComponentId componentId, TriPortId tsiPortId, TriAddress address, TriMessage sendMessage) {
+		System.out.println("PortFilter " + this + " receives following outgoing message: "
 				+ new String(sendMessage.getEncodedMessage()));
 
 		// message can be send out without any modification
--- TTCN3_Project/javasrc/CANPortFilter/CANPortFilterProvider.java
+++ TTCN3_Project/javasrc/CANPortFilter/CANPortFilterProvider.java
@@ -3,6 +3,7 @@ package CANPortFilter;
 import com.testingtech.ttcn.tri.PortFilter;
 import com.testingtech.ttcn.tri.extension.PortFilterProvider;
 
+// FIXME: This whole class does nothing except returning an object of type CANPportFilter(). So: Why is it necessary?
 public class CANPortFilterProvider implements PortFilterProvider {
 
 	@Override
