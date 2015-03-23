94ac78805664f440c8889c8a2edddf78d843cf46
chrisdamm <christian_damm@hotmail.de>
2015-03-23 15:53:23 +0100

    add structure for str2float codec

---
+++ TTCN3_Project/javasrc/codec/Str2Float.java
@@ -0,0 +1,23 @@
+package codec;
+
+import org.etsi.ttcn.tci.Type;
+import org.etsi.ttcn.tci.Value;
+import org.etsi.ttcn.tri.TriMessage;
+
+import com.testingtech.ttcn.tri.AbstractCodecPlugin;
+
+public class Str2Float extends AbstractCodecPlugin{
+
+	@Override
+	public Value decode(TriMessage message, Type decodingHypothesis) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TriMessage encode(Value value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
