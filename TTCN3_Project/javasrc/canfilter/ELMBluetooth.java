44661875b2a7f8411b86e8da4f8395e76904a7ae
fukuehl <c.kuehling@gmail.com>
2015-03-24 13:13:59 +0100

    error-handling + div

--- TTCN3_Project/javasrc/canfilter/ELMBluetooth.java
+++ TTCN3_Project/javasrc/canfilter/ELMBluetooth.java
@@ -7,7 +7,10 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import java.util.HashMap;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Vector;
 
 import javax.bluetooth.DeviceClass;
@@ -28,13 +31,16 @@ public class ELMBluetooth implements DiscoveryListener {
 
   private static String connectionURL = null;
   
-  public static HashMap<String, String> openXCToOBD2Map = new HashMap<String, String>();
+  public static Hashtable<String, String> openXCToOBD2Map = new Hashtable<String, String>();
+  
+  private static Hashtable<String, Car2XEntry> car2xEntries = new Hashtable<>();
   /**
    * @param String[]
    */
   public static void main(String[] args) throws IOException,
       InterruptedException {
     initOpenXCToOBD2Map();
+    initCar2XEntries();
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     ELMBluetooth obj = new ELMBluetooth();
@@ -107,10 +113,18 @@ public class ELMBluetooth implements DiscoveryListener {
             .println("Remember to set the Protocol to auto: AT SP 0  , to check AT DP");
         while (true) {
           System.out.println("Enter Command: ");
-          String command = br.readLine();
-          pwriter.write(command + "\r");
-          pwriter.flush();
-          System.out.println("command send: " + command + " = " + openXCToOBD2Map.get(command));
+          String command = br.readLine(); 
+          if (checkIfHexOrAT(command) || command.equals("")) {
+            pwriter.write(command + "\r");
+            pwriter.flush();
+            System.out.println("command send: " + command + " = " 
+                + getKeyByValue(openXCToOBD2Map,command));
+          } else if (car2xEntries.containsKey(command)) {
+            pwriter.write(car2xEntries.get(command).getObd2key() + "\r");
+            pwriter.flush();
+            System.out.println("command send: " + car2xEntries.get(command).getObd2key() 
+                + " = " + command);
+          }
           Thread.sleep(500);
           byte bytes = 0;
           StringBuilder res = new StringBuilder();
@@ -138,8 +152,20 @@ public class ELMBluetooth implements DiscoveryListener {
            * carriage return before those two (trimmed above).
            */
           rawData = rawData.substring(rawData.lastIndexOf(13) + 1);
+          
+          
+          if (checkIfHexOrAT(command) || command.equals("")) {
+            if (getKeyByValue(openXCToOBD2Map,command) != null) {
+              System.out.println("response: " + rawData + " = " 
+                  + convertOBD2ReplyToOpenXC( getKeyByValue(openXCToOBD2Map,command), rawData));
+            } else {
+              System.out.println("response: " + rawData + "    (not used in any Testcase)");
+        	}
+          } else if(car2xEntries.containsKey(command)) {
+            System.out.println("response:" + rawData + " = " 
+               + convertOBD2ReplyToOpenXC(command, rawData));
+          }
 
-          System.out.println("response: " + rawData);
         }
       }
     }
@@ -200,6 +226,7 @@ public class ELMBluetooth implements DiscoveryListener {
    * @return
    */
   public static void initOpenXCToOBD2Map() {  
+    //TODO Error-Handling if there is no OBD2 Key(here XX)
     openXCToOBD2Map.put("vehicle_speed", "01 0D");
     openXCToOBD2Map.put("engine_speed", "01 0C");
     openXCToOBD2Map.put("steering_wheel_angle", "XX");
@@ -236,6 +263,53 @@ public class ELMBluetooth implements DiscoveryListener {
     openXCToOBD2Map.put("engine_coolant_temperature", "01 05");
     openXCToOBD2Map.put("throttle_position", "01 11");
   }
+  
+  /**
+   * 
+   */
+  public static void initCar2XEntries() {
+    car2xEntries.put("vehicle_speed", new Car2XEntry(0));
+    car2xEntries.put("engine_speed", new Car2XEntry(0));
+    car2xEntries.put("steering_wheel_angle", new Car2XEntry(0));
+    car2xEntries.put("torque_at_transmission", new Car2XEntry(0));
+    car2xEntries.put("accelerator_pedal_position", new Car2XEntry(0));
+    car2xEntries.put("parking_brake_status", new Car2XEntry(0));
+    car2xEntries.put("brake_pedal_status", new Car2XEntry(0));
+    car2xEntries.put("transmission_gear_position", new Car2XEntry(0));
+    car2xEntries.put("gear_lever_position", new Car2XEntry(0));
+    car2xEntries.put("odometer", new Car2XEntry(0));
+    car2xEntries.put("ignition_status", new Car2XEntry(0));
+    car2xEntries.put("fuel_level", new Car2XEntry(0));
+    car2xEntries.put("fuel_consumed_since_restart", new Car2XEntry(0));
+    car2xEntries.put("door_status", new Car2XEntry(0));
+    car2xEntries.put("headlamp_status", new Car2XEntry(0));
+    car2xEntries.put("high_beam_status", new Car2XEntry(0));
+    car2xEntries.put("windshield_wiper_status", new Car2XEntry(0));
+    car2xEntries.put("latitude", new Car2XEntry(0));
+    car2xEntries.put("longitude", new Car2XEntry(0));
+    car2xEntries.put("battery_status", new Car2XEntry(0));
+    car2xEntries.put("absolute_load", new Car2XEntry(0));
+    car2xEntries.put("runtime_since_last_start", new Car2XEntry(0));
+    car2xEntries.put("timing_advanced", new Car2XEntry(0));
+    car2xEntries.put("engine_oil_temperature", new Car2XEntry(0));
+    car2xEntries.put("drivers_demand_engine", new Car2XEntry(0));
+    car2xEntries.put("actual_engine", new Car2XEntry(0));
+    car2xEntries.put("engine_reference", new Car2XEntry(0));
+    car2xEntries.put("fuel_consumption_rate", new Car2XEntry(0));
+    car2xEntries.put("barometric_pressure", new Car2XEntry(0));
+    car2xEntries.put("fuel_pressure", new Car2XEntry(0));
+    car2xEntries.put("Intake_manifold_absolute_pressure", new Car2XEntry(0));
+    car2xEntries.put("air_intake_temperature", new Car2XEntry(0));
+    car2xEntries.put("ambient_air_temperature", new Car2XEntry(0));
+    car2xEntries.put("engine_coolant_temperature", new Car2XEntry(0));
+    car2xEntries.put("throttle_position", new Car2XEntry(0)); 
+
+    Enumeration<String> enumKey = car2xEntries.keys();
+    while(enumKey.hasMoreElements()) {
+      String key = enumKey.nextElement();
+      car2xEntries.get(key).setObds2key(openXCToOBD2Map.get(key));
+    }
+  }
   /**
    * 
    * @param type
@@ -244,109 +318,130 @@ public class ELMBluetooth implements DiscoveryListener {
    */
   public static String convertOBD2ReplyToOpenXC(String type, String reply) {
     String result;
-
-    String[] data = reply.split("\\s"); // cut first 4 bytes (response =
-    // "41 0C 0C FC") 4,1,0,C,0,C,F,C
-    int value1;
-    int value2;
-    int resultInt;
-    switch (type) {
-      case "vehicle_speed":
-        value1 = Integer.parseInt(data[2] + data[3], 16);
-        result = Integer.toString(value1);
-        break;
-      case "engine_speed": // ((A*256)+B)/4
-        value1 = Integer.parseInt(data[2] + data[3], 16);
-        value2 = Integer.parseInt(data[4] + data[5], 16);
-        resultInt = ((value1 * 256) + value2) / 4;
-        result = Integer.toString(resultInt);
-        break;
-      case "accelerator_pedal_position":
-        result = (new Integer(
-          Integer.parseInt(data[2] + data[3], 16) * 100 / 255))
-          .toString();
-        break;
-      case "fuel_level":
-        result = (new Integer(
-          Integer.parseInt(data[2] + data[3], 16) * 100 / 255))
-          .toString();
-        break;
-      case "battery_status": // 11.8V
-        result = data[0] + data[1] + data[2] + data[3];
-        break;
-      case "absolute_load":
-        value1 = Integer.parseInt(data[2] + data[3], 16);
-        value2 = Integer.parseInt(data[4] + data[5], 16);
-        resultInt = ((value1 * 256) + value2) * 100 / 255;
-        result = Integer.toString(resultInt);
-        break;
-      case "runtime_since_last_start":
-        value1 = Integer.parseInt(data[2] + data[3], 16);
-        value2 = Integer.parseInt(data[4] + data[5], 16);
-        resultInt = ((value1 * 256) + value2);
-        result = Integer.toString(resultInt);
-        break;
-      case "timing_advanced": // relative to #1 cylinder
-        result = Integer
-          .toString((Integer.parseInt(data[2] + data[3], 16) - 128) / 2);
-        break;
-      case "engine_oil_temperature":
-        result = Integer
-          .toString(Integer.parseInt(data[2] + data[3], 16) - 40);
-        break;
-      case "drivers_demand_engine": // percentage torque
-        result = Integer
-          .toString(Integer.parseInt(data[2] + data[4], 16) - 125);
-        break;
-      case "actual_engine": // percentage torque
-        result = Integer
-          .toString(Integer.parseInt(data[2] + data[3], 16) - 125);
-        break;
-      case "engine_reference": // torque Nm
-        value1 = Integer.parseInt(data[2] + data[3], 16);
-        value2 = Integer.parseInt(data[4] + data[5], 16);
-        resultInt = ((value1 * 256) + value2);
-        result = Integer.toString(resultInt);
-        break;
-      case "fuel_consumption_rate": // engine_fuel_rate
-        value1 = Integer.parseInt(data[2] + data[3], 16);
-        value2 = Integer.parseInt(data[4] + data[5], 16);
-        resultInt = ((value1 * 256) + value2) / 20;
-        result = Integer.toString(resultInt);
-        break;
-      case "barometric_pressure": // kPa
-        result = data[2];
-        break;
-      case "fuel_pressure": // kPa
-        value1 = Integer.parseInt(data[2] + data[3], 16) * 3;
-        result = Integer.toString(value1);
-        break;
-      case "Intake_manifold_absolute_pressure":
-        result = data[2];
-        break;
-      case "air_intake_temperature":
-        value1 = Integer.parseInt(data[2] + data[3], 16) - 40;
-        result = Integer.toString(value1);
-        break;
-      case "ambient_air_temperature":
-        value1 = Integer.parseInt(data[2] + data[3], 16) - 40;
-        result = Integer.toString(value1);
-        break;
-      case "engine_coolant_temperature":
-        value1 = Integer.parseInt(data[2] + data[3], 16) - 40;
-        result = Integer.toString(value1);
-        break;
-      case "throttle_position":
-        value1 = Integer.parseInt(data[2] + data[3], 16) * 100 / 255;
-        result = Integer.toString(value1);
-        break;
-      default:
-        result = reply;
-        break;
+    if (reply.equals("NO DATA") || reply.equals("OK") || reply.startsWith("BUS")) {
+      result = reply;
+    } else {
+     String[] data = reply.split("\\s"); // cut first 4 bytes (response =
+    	// "41 0C 0C FC") 41,0C,0C,FC
+    	int value1;
+    	int value2;
+    	int resultInt;
+    	switch (type) {
+    	case "vehicle_speed":
+    		value1 = Integer.parseInt(data[2], 16);
+    		result = Integer.toString(value1);
+    		break;
+    	case "engine_speed": // ((A*256)+B)/4
+    		value1 = Integer.parseInt(data[2], 16);
+    		value2 = Integer.parseInt(data[3], 16);
+    		resultInt = ((value1 * 256) + value2) / 4;
+    		result = Integer.toString(resultInt);
+    		break;
+    	case "accelerator_pedal_position":
+    		result = (new Integer(
+    				Integer.parseInt(data[2], 16) * 100 / 255))
+    				.toString();
+    		break;
+    	case "fuel_level":
+    		result = (new Integer(
+    				Integer.parseInt(data[2], 16) * 100 / 255))
+    				.toString();
+    		break;
+    	case "battery_status": // 11.8V
+    		result = reply;
+    		break;
+    	case "absolute_load":
+    		value1 = Integer.parseInt(data[2], 16);
+    		value2 = Integer.parseInt(data[3], 16);
+    		resultInt = ((value1 * 256) + value2) * 100 / 255;
+    		result = Integer.toString(resultInt);
+    		break;
+    	case "runtime_since_last_start":
+    		value1 = Integer.parseInt(data[2], 16);
+    		value2 = Integer.parseInt(data[3], 16);
+    		resultInt = ((value1 * 256) + value2);
+    		result = Integer.toString(resultInt);
+    		break;
+    	case "timing_advanced": // relative to #1 cylinder
+    		result = Integer
+    		.toString((Integer.parseInt(data[2], 16) - 128) / 2);
+    		break;
+    	case "engine_oil_temperature":
+    		result = Integer
+    		.toString(Integer.parseInt(data[2], 16) - 40);
+    		break;
+    	case "drivers_demand_engine": // percentage torque
+    		result = Integer
+    		.toString(Integer.parseInt(data[2], 16) - 125);
+    		break;
+    	case "actual_engine": // percentage torque
+    		result = Integer
+    		.toString(Integer.parseInt(data[2], 16) - 125);
+    		break;
+    	case "engine_reference": // torque Nm
+    		value1 = Integer.parseInt(data[2], 16);
+    		value2 = Integer.parseInt(data[3], 16);
+    		resultInt = ((value1 * 256) + value2);
+    		result = Integer.toString(resultInt);
+    		break;
+    	case "fuel_consumption_rate": // engine_fuel_rate
+    		value1 = Integer.parseInt(data[2], 16);
+    		value2 = Integer.parseInt(data[3], 16);
+    		resultInt = ((value1 * 256) + value2) / 20;
+    		result = Integer.toString(resultInt);
+    		break;
+    	case "barometric_pressure": // kPa
+    		result = Integer.toString(Integer.parseInt(data[2], 16));
+    		break;
+    	case "fuel_pressure": // kPa
+    		value1 = Integer.parseInt(data[2], 16) * 3;
+    		result = Integer.toString(value1);
+    		break;
+    	case "Intake_manifold_absolute_pressure":
+    		result = Integer.toString(Integer.parseInt(data[2], 16));
+    		break;
+    	case "air_intake_temperature":
+    		value1 = Integer.parseInt(data[2], 16) - 40;
+    		result = Integer.toString(value1);
+    		break;
+    	case "ambient_air_temperature":
+    		value1 = Integer.parseInt(data[2], 16) - 40;
+    		result = Integer.toString(value1);
+    		break;
+    	case "engine_coolant_temperature":
+    		value1 = Integer.parseInt(data[2], 16) - 40;
+    		result = Integer.toString(value1);
+    		break;
+    	case "throttle_position":
+    		value1 = Integer.parseInt(data[2], 16) * 100 / 255;
+    		result = Integer.toString(value1);
+    		break;
+    	default:
+    		result = reply;
+    		break;
+    	}
     }
 
     return result;
 
+  }
+  
+  public static <T, E> T getKeyByValue(Map<T, E> map, E value) {
+	    for (Entry<T, E> entry : map.entrySet()) {
+	        if (value.equals(entry.getValue())) {
+	            return entry.getKey();
+	        }
+	    }
+	    return null;
+	}
+  
+  public static boolean checkIfHexOrAT(String input){
+	  String rawData = input.replaceAll("\\s", "");
+	    if (rawData.matches("([0-9A-F]{2})+") || (rawData.startsWith("AT"))) {
+	    	return true;
+	    } else {
+	    	return false;
+	    }	  
   }
 
 }
