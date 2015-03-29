c4b73a2ac952f962e8d7baf3cbbd786b9d2d09a0
aredok <b.kodera@fu-berlin.de>
2015-03-30 01:06:23 +0200

    Refactoring: using CAN filter log

--- TTCN3_Project/CANFilter/com/testingtech/playits/canfilter/CANFilterService.java
+++ TTCN3_Project/CANFilter/com/testingtech/playits/canfilter/CANFilterService.java
@@ -30,7 +30,6 @@ package com.testingtech.playits.canfilter;
 
 import java.io.Closeable;
 import java.io.IOException;
-import java.lang.Thread.State;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -44,138 +43,143 @@ import org.json.JSONTokener;
 
 public class CANFilterService {
 
-  private static final int DEFAULT_SERVER_PORT = 7000;
-  private static final String DEFAULT_SERVER_HOST = "localhost";
+	private static final int DEFAULT_SERVER_PORT = 7000;
+	private static final String DEFAULT_SERVER_HOST = "localhost";
 
-  private static Hashtable<String, Car2XEntry> car2xEntries = new Hashtable<>();
-  private static Thread entryUpdaterThread;
-  private static Car2XEntryUpdater entryUpdater;
-  private static TimeoutResponder timeoutResponder;
-  static CANFilterLog filterLog = new CANFilterLog();
+	private static Hashtable<String, Car2XEntry> car2xEntries = new Hashtable<>();
+	private static Car2XEntryUpdater entryUpdater;
+	private static TimeoutResponder timeoutResponder;
+	static CANFilterLog canFilterLog = new CANFilterLog(
+			CANFilterService.class.getSimpleName());
 
-  /**
-   * Provides TCP server functionality to handle JSON requests. Upon receiving
-   * a start request, an entry updater thread which updates openXC / obd2 hash
-   * table entries is started. Parallel timeout responder tasks periodically
-   * send back updated JSON values from the hash table over a client socket.
-   * 
-   * @param args
-   *            host, port
-   */
-  public static void main(String[] args) {
-    String host;
-    int port;
-    
-    if (args.length != 2) {
-      filterLog.logCommandLineUsage();
-      host = DEFAULT_SERVER_HOST;
-      port = DEFAULT_SERVER_PORT;
-    } else {
-      host = PatternMatchUtils.matchHost(args[0], DEFAULT_SERVER_HOST);
-      port = PatternMatchUtils.matchPortNumber(args[1], DEFAULT_SERVER_PORT);
-    }
+	/**
+	 * Provides TCP server functionality to handle JSON requests. Upon receiving
+	 * a start request, an entry updater thread which updates openXC / obd2 hash
+	 * table entries is started. Parallel timeout responder tasks periodically
+	 * send back updated JSON values from the hash table over a client socket.
+	 * 
+	 * @param args
+	 *            host, port
+	 */
+	public static void main(String[] args) {
+		Socket socket = null;
+		ServerSocket serverSocket = null;
+		try {
+			serverSocket = createServerSocket(args);
+			socket = serverSocket.accept();
+			timeoutResponder = new TimeoutResponder(socket, car2xEntries);
+			entryUpdater = new Car2XEntryUpdater(car2xEntries);
+			JSONTokener jsonTokener = new JSONTokener(socket.getInputStream());
+			while (jsonTokener.more()) {
+				try {
+					Object nextValue = jsonTokener.nextValue();
+					if (nextValue instanceof JSONObject) {
+						canFilterLog.logInfo(
+								FilterLogMessages.INCOMING_REQUEST,
+								nextValue.toString());
+						// TODO maybe add a kill request to exit loop
+						handleRequest((JSONObject) nextValue);
+					} else {
+						canFilterLog.logError(FilterLogMessages.JSON_ERROR,
+								nextValue.toString());
+					}
+				} catch (JSONException e) {
+					canFilterLog.logError(FilterLogMessages.JSON_ERROR);
+				}
+			}
 
-    InetSocketAddress address = new InetSocketAddress(host, port);
-    Socket socket = null;
-    ServerSocket serverSocket = null;
-    try {
-      serverSocket = createServerSocket(address);
-      filterLog.logStartListening(serverSocket.getInetAddress().getHostName(), serverSocket.getLocalPort());
-      
-      socket = serverSocket.accept();
-      timeoutResponder = new TimeoutResponder(socket, car2xEntries);
-      entryUpdater = new Car2XEntryUpdater(car2xEntries);
-      JSONTokener jsonTokener = new JSONTokener(socket.getInputStream());
-      while (jsonTokener.more()) {
-        try {
-          Object nextValue = jsonTokener.nextValue();
-          if (nextValue instanceof JSONObject) {
-            filterLog.logIncomingRequest(nextValue.toString());
-            // TODO maybe add a kill request to exit loop
-            handleRequest((JSONObject) nextValue);
-          } else {
-            filterLog.logJSONError(nextValue.toString());
-          }
-        } catch (JSONException e) {
-          filterLog.logJSONError(e.getMessage());
-        }
-      }
+		} catch (IOException e) {
+			canFilterLog.logError(FilterLogMessages.SOCKET_ERROR);
+		} catch (JSONException e) {
+			canFilterLog.logError(FilterLogMessages.JSON_ERROR);
+		} finally {
+			canFilterLog.logInfo(FilterLogMessages.STOPP);
+			close(socket);
+			close(serverSocket);
+		}
+	}
 
-    } catch (IOException e) {
-      filterLog.logSocketError(e.getMessage());
-    } catch (JSONException e) {
-      filterLog.logJsonErrror();
-    } finally {
-      filterLog.logNoMoreRequests();
-      close(socket);
-      close(serverSocket);
-    }
-  }
+	final static String HOST_PATTERN = "(localhost|(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[0-9]{1,2})(\\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[0-9]{1,2})){3})";
+	final static String PORT_NUMBER_PATTERN = "(6553[0-5]|655[0-2]\\d|65[0-4]\\d\\d|6[0-4]\\d{3}|[1-5]\\d{4}|[1-9]\\d{0,3}|0)";
+	
+	private static InetSocketAddress createAddress(String[] args) {
+		String host;
+		int port;
+		if (args.length != 2) {
+			canFilterLog.logError(FilterLogMessages.WRONG_CMD_LINE_USAGE);
+			host = DEFAULT_SERVER_HOST;
+			port = DEFAULT_SERVER_PORT;
+		} else {
+			host = args[0].matches(HOST_PATTERN) ? args[0] : DEFAULT_SERVER_HOST;
+			port = args[1].matches(PORT_NUMBER_PATTERN) ? Integer.valueOf(args[1]) : DEFAULT_SERVER_PORT;
+		}
+		return new InetSocketAddress(host, port);
+	}
 
-  private static void close(Closeable socket) {
-    try {
-      if (socket != null) {
-        socket.close();
-      }
-    } catch (IOException e) {
-      filterLog.logSocketClose(e.getMessage());
-    }
-  }
+	private static void close(Closeable socket) {
+		try {
+			if (socket != null) {
+				socket.close();
+			}
+		} catch (IOException e) {
+			canFilterLog.logError(FilterLogMessages.SOCKET_ERROR,
+					e.getMessage());
+		}
+	}
 
-  private static ServerSocket createServerSocket(InetSocketAddress address)
-      throws IOException, SocketException {
-    ServerSocket serverSocket = new ServerSocket();
-    serverSocket.setReuseAddress(true);
-    serverSocket.bind(address);
-    return serverSocket;
-  }
+	private static ServerSocket createServerSocket(String[] args)
+			throws IOException, SocketException {
+		ServerSocket serverSocket = new ServerSocket();
+		serverSocket.setReuseAddress(true);
+		InetSocketAddress address = createAddress(args);
+		serverSocket.bind(address);
+		canFilterLog.logInfo(FilterLogMessages.START, address.getHostName(), String.valueOf(address.getPort()));
+		return serverSocket;
+	}
 
-  private static void handleRequest(JSONObject request) throws IOException,
-      JSONException {
-    JSONArray data = request.getJSONArray("reqData");
-    switch (request.getString("reqType")) {
-    case "start":
-      addEntries(data);
-      break;
-    case "stop":
-      removeEntries(data);
-      break;
-    default:
-      filterLog.logUnsupportedRequest();
-      break;
-    }
-  }
+	private static void handleRequest(JSONObject request) throws IOException,
+			JSONException {
+		JSONArray data = request.getJSONArray("reqData");
+		switch (request.getString("reqType")) {
+		case "start":
+			addEntries(data);
+			break;
+		case "stop":
+			removeEntries(data);
+			break;
+		default:
+			canFilterLog.logError(FilterLogMessages.UNSUPPORTED_REQUEST);
+			break;
+		}
+	}
 
-  private static void addEntries(JSONArray jsonArray) throws JSONException {
-    for (int index = 0; index < jsonArray.length(); index++) {
-      JSONObject jsonObject = jsonArray.getJSONObject(index);
-      addEntry(jsonObject.getString("key"), jsonObject.getInt("interval"));
-    }
-  }
+	private static void addEntries(JSONArray jsonArray) throws JSONException {
+		for (int index = 0; index < jsonArray.length(); index++) {
+			JSONObject jsonObject = jsonArray.getJSONObject(index);
+			addEntry(jsonObject.getString("key"), jsonObject.getInt("interval"));
+		}
+	}
 
-  private static void addEntry(String key, int interval) {
-    filterLog.logEntryAddition(key, interval);
-    car2xEntries.put(key, new Car2XEntry());
+	private static void addEntry(String key, int interval) {
+		canFilterLog.logInfo(FilterLogMessages.ENTRY_ADDED, key);
+		car2xEntries.put(key, new Car2XEntry());
 
-    if (entryUpdaterThread == null
-        || entryUpdaterThread.getState() != State.RUNNABLE) {
-      filterLog.logStartEntryUpdater();
-      entryUpdaterThread = new Thread(entryUpdater);
-      entryUpdaterThread.start();
-    }
-    timeoutResponder.addTimer(key, interval);
-  }
+		if (!entryUpdater.isRunning()) {
+			new Thread(entryUpdater).start();
+		}
+		timeoutResponder.addTimer(key, interval);
+	}
 
-  private static void removeEntries(JSONArray jsonArray) throws JSONException {
-    for (int index = 0; index < jsonArray.length(); index++) {
-      JSONObject jsonObject = jsonArray.getJSONObject(index);
-      removeEntry(jsonObject.getString("key"));
-    }
-  }
+	private static void removeEntries(JSONArray jsonArray) throws JSONException {
+		for (int index = 0; index < jsonArray.length(); index++) {
+			JSONObject jsonObject = jsonArray.getJSONObject(index);
+			removeEntry(jsonObject.getString("key"));
+		}
+	}
 
-  private static void removeEntry(String key) {
-    filterLog.logEntryRemoval(key);
-    car2xEntries.remove(key);
-    timeoutResponder.removeTimer(key);
-  }
+	private static void removeEntry(String key) {
+		canFilterLog.logInfo(FilterLogMessages.ENTRY_REMOVED, key);
+		car2xEntries.remove(key);
+		timeoutResponder.removeTimer(key);
+	}
 }
