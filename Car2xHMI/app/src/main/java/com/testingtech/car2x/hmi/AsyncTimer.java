308defb2ca51cca65a2b1ad471c55de8512dc963
FlyingCrafter <paul.kunze@fu-berlin.de>
2015-03-31 20:06:30 +0200

    Finished the timer implementation

--- Car2xHMI/app/src/main/java/com/testingtech/car2x/hmi/AsyncTimer.java
+++ Car2xHMI/app/src/main/java/com/testingtech/car2x/hmi/AsyncTimer.java
@@ -2,21 +2,24 @@ package com.testingtech.car2x.hmi;
 
 import android.graphics.Color;
 import android.os.AsyncTask;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class AsyncTimer extends AsyncTask<Void, Integer, Void> {
 
     private TextView noticeText;
     private int time;
+    private Button btnStop;
 
-    public AsyncTimer(TextView text, int t){
+    public AsyncTimer(Button btnStop, TextView text, int t){
+        this.btnStop = btnStop;
         this.noticeText = text;
         this.time = t;
     }
 
     @Override
     protected Void doInBackground(Void... params) {
-        for (int t = time; t > 0; t--) {
+        for (int t = time; t >= 0; t--) {
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException ie) {
@@ -32,21 +35,22 @@ public class AsyncTimer extends AsyncTask<Void, Integer, Void> {
     @Override
     protected void onProgressUpdate(Integer... result) {
         super.onProgressUpdate(result);
-        noticeText.setTextColor(Color.YELLOW);
-        noticeText.setText("Time left: " + time);
+        noticeText.setBackgroundColor(Color.YELLOW);
+        noticeText.setText(" Time left: " + result[0] + " ");
     }
 
     @Override
     protected void onCancelled(){
         // test succeeded in time
-        noticeText.setTextColor(Color.TRANSPARENT);
+        noticeText.setBackgroundColor(Color.TRANSPARENT);
         noticeText.setText("");
     }
 
     @Override
     protected void onPostExecute(Void result) {
         // test time expired
-        noticeText.setTextColor(Color.RED);
-        noticeText.setText("The time expired. The test failed.");
+        noticeText.setBackgroundColor(Color.RED);
+        noticeText.setText(" The time expired. The test failed. ");
+        btnStop.callOnClick();
     }
 }
