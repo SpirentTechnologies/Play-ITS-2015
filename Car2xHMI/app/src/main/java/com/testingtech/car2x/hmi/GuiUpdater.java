5c269ac24a2c8e11840257be8e0224a22b40929b
FlyingCrafter <paul.kunze@fu-berlin.de>
2015-04-02 10:09:37 +0200

    Removed the settings button stub

--- Car2xHMI/app/src/main/java/com/testingtech/car2x/hmi/GuiUpdater.java
+++ Car2xHMI/app/src/main/java/com/testingtech/car2x/hmi/GuiUpdater.java
@@ -38,7 +38,6 @@ public class GuiUpdater extends Activity{
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                //noticeText.setText("");
                 noticeText.setBackgroundColor(Color.TRANSPARENT);
                 statusRunningText.setText(R.string.textview_not_running);
                 for (int i = 0; i < table.getChildCount(); i++){
